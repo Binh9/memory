d2db0009804f651fcca7d93c88c86de23a050fd7
memory <memory@vultr.guest>
2019-02-08 22:59:22 +0000

    ielixir logi

---
+++ lib/memory/game.ex
@@ -0,0 +1,84 @@
+defmodule Memory.Game do
+
+  def new do
+    state = 
+    %{
+      tiles: [],
+      tilesLeft: 16, 
+      numClicks: 0,
+      openID: -1
+     }
+
+     values = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+     randValues = Enum.shuffle(values)
+      |> Enum.with_index()
+
+     outArr = Enum.map(randValues, fn {x, id} -> %{id: id, value: x, visible: false, done: false} end)
+
+     Map.put(state, :tiles, outArr)
+  end
+
+  def client_view(game) do
+    %{
+      tiles: game.tiles,
+      tilesLeft: game.tilesLeft,
+      numClicks: game.numClicks,
+      openID: game.openID
+    }
+  end
+
+  # For reseting
+  def reset(game) do
+    new()
+  end
+
+  # Reveal the clicked tile
+  def reveal(game, id) do
+    # Get the tile at the given id
+    tile = Enum.at(game.tiles, id)
+    tile = Map.put(tile, :visible, true)
+    # Replace with the updated tile
+    newTiles = List.replace_at(game.tiles, id, tile)
+    game = Map.put(game, :tiles, newTiles)
+  end
+
+  def guess(game, id) do
+    if (game.openID == -1) do
+      # First tile is selected
+      game = Map.put(game, :openID, id)
+        |> Map.put(:numClicks, game.numClicks + 1)
+    else
+      # Second tile is selected
+      # TODO + screen freezing
+      game = handleMatching(game, id, game.openID)
+        |> Map.put(:numClicks, game.numClicks + 1)
+    end
+  end
+
+  # Given the two different ids, determines if there is matching or not
+  # and performs corresponding actions
+  def handleMatching(game, id1, id2) do
+    tile1 = Enum.at(game.tiles, id1)
+    tile2 = Enum.at(game.tiles, id2)
+    if (tile1.value == tile2.value) do
+      # Good match
+      tile1 = Map.put(tile1, :done, true)
+      tile2 = Map.put(tile2, :done, true)
+      newTiles = List.replace_at(game.tiles, id1, tile1)
+        |> List.replace_at(id2, tile2)
+
+      game = Map.put(game, :tiles, newTiles)
+        |> Map.put(:tilesLeft, game.tilesLeft - 2)
+    else
+      # Bad match
+      tile1 = Map.put(tile1, :visible, false)
+      tile2 = Map.put(tile2, :visible, false)
+      newTiles = List.replace_at(game.tiles, id1, tile1)
+        |> List.replace_at(id2, tile2)
+
+      game = Map.put(game, :tiles, newTiles)
+    end
+  end
+
+
+end 
---
+++ lib/memory_web/channels/games_channel.ex
@@ -0,0 +1,64 @@
+defmodule MemoryWeb.GamesChannel do
+  use MemoryWeb, :channel
+
+  alias Memory.Game
+
+
+  def join("games:" <> name, payload, socket) do
+    if authorized?(payload) do
+      game = BackupAgent.get(name) || Game.new()
+      socket = socket
+      |> assign(:game, game)
+      |> assign(:name, name)
+      BackupAgent.put(name, game)
+      {:ok, %{"join" => name, "game" => Game.client_view(game)}, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  #def handle_in("guess", %{"letter" => ll}, socket) do
+  #  name = socket.assigns[:name]
+  #  game = Game.guess(socket.assigns[:game], ll)
+  #  socket = assign(socket, :game, game)
+  #  BackupAgent.put(name, game)
+  #  {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
+  #end
+
+  # For reset
+  def handle_in("reset", payload, socket) do
+    name = socket.assigns[:name]
+    game = Game.reset()
+    socket = assign(socket, :game, game)
+    BackupAgent.put(name, game)
+    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
+  end
+  
+  # When the tile is clicked
+  def handle_in("reveal", %{"id" => id}, socket) do
+    name = socket.assigns[:name]
+    game = Game.reveal(socket.assigns[:game], id)
+    socket = assign(socket, :game, game)
+    BackupAgent.put(name, game)
+    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}  
+  end
+
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (games:lobby).
+  def handle_in("shout", payload, socket) do
+    broadcast socket, "shout", payload
+    {:noreply, socket}
+  end
+
+  # TODO Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
+  end
+end
